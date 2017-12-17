7eb92a9f571f863a0dcfdfb43c09de53fa7407b0
Ke Sun <ke.sun.x@gmail.com>
2017-12-17 23:34:43 +0100

    add use cases of Functional Interface

--- code/eclipse/java.sample.thread/src/sample/lambda/UseCase.java
+++ code/eclipse/java.sample.thread/src/sample/lambda/UseCase.java
@@ -5,7 +5,11 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.Consumer;
 
 public class UseCase {
 	static int globalCounter = 0;
@@ -13,7 +17,7 @@ public class UseCase {
 	
 	List<String> names = Arrays.asList("Peugeot", "Citroen", "Renault");
 
-	void Compare1 () {
+	void Comparator () {
 		Collections.sort(names, new Comparator<String> () {
 			@Override
 			public int compare(String a, String b) {
@@ -22,14 +26,14 @@ public class UseCase {
 		});
 	}
 	
-	void Compare2 () {
+	void VariousLambda () {
 		Collections.sort(names, (String a, String b) -> { return a.compareTo(b); });
 		Collections.sort(names, (a, b) -> { return a.compareTo(b); });
 		Collections.sort(names, (String a, String b) -> a.compareTo(b));
 		Collections.sort(names, (a, b) -> a.compareTo(b));
 	}
 	
-	void Compare3 () {
+	void LambdaScope () {
 		// at least i must be implicitly final, i.e. its value cannot be modified
 		int i = 1;
 		Converter<String, Integer> converter = (origin) -> {
@@ -42,7 +46,7 @@ public class UseCase {
 		System.out.println(converter.convert("123"));
 	}
 	
-	void Compare4 () {
+	void MethodReference () {
 		// use static method reference
 		Converter<String, Integer> converter = Integer::valueOf;
 		System.out.println(converter.convert("123"));
@@ -61,5 +65,32 @@ public class UseCase {
 		// isEmpty() is used like extension method in Xtend
 		Predicate<String> isEmpty = String::isEmpty;
 		Predicate<String> notEmpty = isEmpty.negate(); 
+	}
+	
+	void Function () {
+		Function <String, Integer> toInteger = Integer::valueOf;
+		Function<String, String> backToString = toInteger.andThen(String::valueOf);
+		backToString.apply("123");	// "123"
+	}
+	
+	void Supplier () {
+		Supplier<String> stringSupplier = String::new;
+		stringSupplier.get();	// new String instance
+	}
+	
+	void Consumer () {
+		Consumer<String> greeter = (p) -> System.out.println("The length is " + p.length());
+		greeter.accept("Republic"); // The length is 8
+	}
+	
+	void Optional () {
+		// Optional is not functional interface
+		Optional<String> optional = Optional.of("foo");
+
+		optional.isPresent();           // true
+		optional.get();                 // "foo"
+		optional.orElse("fallback");    // "foo"
+
+		optional.ifPresent((s) -> System.out.println(s.charAt(0)));     // "f"
 	}
 }
