5bb5a6661bb6092c1765f63d33b849a6f506eef3
Ke Sun <ke.sun.x@gmail.com>
2019-02-27 23:05:33 +0100

    Inner bean injection

--- code/eclipse/sk.spring/src/sk/spring/Speaker.java
+++ code/eclipse/sk.spring/src/sk/spring/Speaker.java
@@ -1,15 +1,19 @@
 package sk.spring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Speaker {
 	String name;
-	Message message;
+	List<Message> messages;
 	
-	public Speaker (String name, Message message) {
+	public Speaker (String name, Message messageAlpha, Message messageBeta) {
 		this.name = name;
-		this.message = message;
+		this.messages = Arrays.asList(messageAlpha, messageBeta);
 	}
 	
 	public void speak () {
-		System.out.println("Speaker " + name + ": " + message.getMessage());
+		System.out.println("Speaker " + name + ": " + (messages.stream().map(item -> item.getMessage()).reduce((acc, item) -> acc + " and " + item).get()));
 	}
 }
